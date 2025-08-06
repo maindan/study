b24449bba0e9d6a2f23947544b10d58f228f90db
Daniel <danielima.projex@gmail.com>
2025-08-06 15:49:41 -0400

    feat: implementado web security

--- src/main/java/com/study/study/config/OpenAPI.java
+++
@@ -1,9 +0,0 @@
-package com.study.study.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class OpenAPI {
-
-}
---
+++ src/main/java/com/study/study/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.study.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig  {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sac API")
+                        .version("1.0")
+                        .description("Documentação da API com Spring Security"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) // Adiciona segurança globalmente
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
