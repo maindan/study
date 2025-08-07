677941bbf89fd80b8536fa68eecc5a621fcf709e
Daniel <danielima.projex@gmail.com>
2025-08-07 09:02:10 -0400

    feat: add auth and user creation

--- src/main/java/com/study/study/controller/UserController.java
+++ src/main/java/com/study/study/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.study.study.controller;
 
+import com.study.study.DTO.AuthLoginDTO;
+import com.study.study.DTO.AuthResponseDTO;
+import com.study.study.DTO.UserCreateDTO;
+import com.study.study.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/user/create")
+    public ResponseEntity createUser(UserCreateDTO data) {
+        userService.create(data);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/auth")
+    public ResponseEntity<AuthResponseDTO> auth(AuthLoginDTO data) {
+        return ResponseEntity.ok().body(userService.login(data));
+    }
 }
--- src/main/java/com/study/study/service/UserService.java
+++ src/main/java/com/study/study/service/UserService.java
@@ -1,4 +1,41 @@
 package com.study.study.service;
 
+import com.study.study.DTO.AuthLoginDTO;
+import com.study.study.DTO.AuthResponseDTO;
+import com.study.study.DTO.UserCreateDTO;
+import com.study.study.config.security.JwtTokenService;
+import com.study.study.config.security.SecurityConfig;
+import com.study.study.config.security.UserDetailsImplementation;
+import com.study.study.model.User;
+import com.study.study.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenService jwtTokenService;
+    private final SecurityConfig securityConfig;
+
+    public void create(UserCreateDTO data) {
+        User user = new User();
+        user.setEmail(data.email());
+        user.setPassword(securityConfig.passwordEncoder().encode(data.password()));
+        userRepository.save(user);
+    }
+
+    public AuthResponseDTO login(AuthLoginDTO data) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(data.email(), data.password());
+
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        UserDetailsImplementation userDetails = (UserDetailsImplementation) authentication.getPrincipal();
+        return new AuthResponseDTO(jwtTokenService.generateToken(userDetails));
+    }
+
 }
